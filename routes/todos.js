64b8ee9436ebb0ab124b20731660ab895ae521b1
katharina-spiecker <kathispiecker@gmail.com>
2024-10-17 16:42:00 +0200

    rewrite to use routes

--- routes/todos.js
+++ routes/todos.js
@@ -1,54 +1,76 @@
 import { Router } from "express";
+import { ObjectId } from 'mongodb';
+import { connectDB } from "../db.js";
 
 const router = Router();
 
-const todos = [
-  {id: 1, name: "Bürgeramt Termin", completed: false},
-  {id: 2, name: "Clean", completed: false}
-];
-
-router.get("/", (req, res) => {
-  res.json(todos)
+router.get("/", async (req, res) => {
+  try {
+    const database = await connectDB();
+    const todosCollection = database.collection("todos");
+    const data = await todosCollection.find().toArray();
+    res.json(data)
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
 });
 
-router.post("/", (req, res) => {
-  const newTodo = req.body;
-  if (!req.body.name) {
-    return res.status(400).send("Missing required field: 'name'");
-  }
+router.get("/:id", async (req, res) => {
+  try {
+    const database = await connectDB();
+    const todosCollection = database.collection("todos");
 
-  todos.push(newTodo);
-  res.status(201).send("Todo created");
-})
-
-router.delete("/:id", (req, res) => {
-  const id = parseInt(req.params.id);
-  const todoIndex = todos.findIndex(todo => todo.id === id);
-
-  if (todoIndex === -1) {
-    res.status(404).send("Todo not found")
-  } else {
-    todos.splice(todoIndex, 1);
-    res.send("Todo deleted");
+    const data = await todosCollection.findOne({_id: new ObjectId(req.params.id)});
+    res.json(data);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
   }
 })
 
-router.put("/:id", (req, res) => {
+router.post("/", async (req, res) => {
   const newTodo = req.body;
-  const todoId = parseInt(req.params.id);
-
-  if (!req.body.name) {
-    return res.status(400).send("Request body must contain name");
-  }
   
-  const todoIndex = todos.findIndex(todo => todo.id === todoId);
+  try {
+    const database = await connectDB();
+    const todosCollection = database.collection("todos");
 
-  if (todoIndex === -1) {
-    return res.status(404).send("Todo not found");
-  } else {
-    todos[todoIndex] = newTodo;
-    res.send("Todo was updated");
-  } 
+    const data = await todosCollection.insertOne(newTodo);
+    res.json({message: "insertion successful", insertedId: data.insertedId});
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+})
+
+router.delete("/:id", async (req, res) => {
+  const id = req.params.id;
+  try {
+      const database = await connectDB();
+      const todosCollection = database.collection("todos");
+
+      const data = await todosCollection.deleteOne({_id: new ObjectId(id)});
+      res.json(data);
+  } catch (error) {
+      res.status(500).json({ error: error.message });
+  }
+})
+
+router.put("/:id", async (req, res) => {
+  try {
+    const database = await connectDB();
+    const todosCollection = database.collection("todos");
+
+    const updatedTodo = req.body;
+    const data = await todosCollection.updateOne({_id: new ObjectId(req.params.id)}, {$set: updatedTodo})
+
+    if (data.matchedCount === 1) {
+      res.json({message: "insertion successful", data: data});
+    } else {
+      res.status(404).json({message: "Todo not found"})
+    }
+    
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
 })
 
 export default router;
--- server.js
+++ server.js
@@ -1,79 +1,11 @@
 import express from "express";
-import { ObjectId } from 'mongodb';
-import { connectDB } from "./db.js";
+import todoRouter from "./routes/todos.js";
 
 const app = express();
 const port = process.env.PORT || 5000;
 
 app.use(express.json());
 
-app.get("/api/todos", async (req, res) => {
-  try {
-    const database = await connectDB();
-    const todosCollection = database.collection("todos");
-    const data = await todosCollection.find().toArray();
-    res.json(data)
-  } catch (error) {
-    res.status(500).json({ error: error.message });
-  }
-});
-
-app.get("/api/todos/:id", async (req, res) => {
-  try {
-    const database = await connectDB();
-    const todosCollection = database.collection("todos");
-
-    const data = await todosCollection.findOne({_id: new ObjectId(req.params.id)});
-    res.json(data);
-  } catch (error) {
-    res.status(500).json({ error: error.message });
-  }
-})
-
-app.post("/api/todos", async (req, res) => {
-  const newTodo = req.body;
-  
-  try {
-    const database = await connectDB();
-    const todosCollection = database.collection("todos");
-
-    const data = await todosCollection.insertOne(newTodo);
-    res.json({message: "insertion successful", insertedId: data.insertedId});
-  } catch (error) {
-    res.status(500).json({ error: error.message });
-  }
-})
-
-app.delete("/api/todos/:id", async (req, res) => {
-  const id = req.params.id;
-  try {
-      const database = await connectDB();
-      const todosCollection = database.collection("todos");
-
-      const data = await todosCollection.deleteOne({_id: new ObjectId(id)});
-      res.json(data);
-  } catch (error) {
-      res.status(500).json({ error: error.message });
-  }
-})
-
-app.put("/api/todos/:id", async (req, res) => {
-  try {
-    const database = await connectDB();
-    const todosCollection = database.collection("todos");
-
-    const updatedTodo = req.body;
-    const data = await todosCollection.updateOne({_id: new ObjectId(req.params.id)}, {$set: updatedTodo})
-
-    if (data.matchedCount === 1) {
-      res.json({message: "insertion successful", data: data});
-    } else {
-      res.status(404).json({message: "Todo not found"})
-    }
-    
-  } catch (error) {
-    res.status(500).json({ error: error.message });
-  }
-})
+app.use("/api/todos", todoRouter);
 
 app.listen(port, () => console.log(`Server is listening on port ${port}`));
